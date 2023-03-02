30e6018c58e1438f5fdc7bbd00461494ae240bab
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2023-03-02 20:34:59 +0300

    Add files via upload

---
+++ Printing.py
@@ -0,0 +1,11 @@
+
+first_name = "SHJA"
+last_name = "Azimi"
+skills = "I know Java , Html , Css , and javaScript"
+age = 18
+
+
+print("Hi i'am  " + first_name + " " + last_name)
+print(age+1)
+print(skills)
+print("Pyton is the one the best program languages.")
